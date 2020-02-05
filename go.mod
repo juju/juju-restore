350a708cad27a3cec144883d00770d80f3746c55
Christian Muirhead <christian.muirhead@canonical.com>
2020-02-05 15:43:41 +1300

    Flag and arg parsing for connecting to mongo

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/juju/juju-restore
+
+go 1.12
+
+require (
+	github.com/juju/ansiterm v0.0.0-20180109212912-720a0952cc2a // indirect
+	github.com/juju/clock v0.0.0-20190205081909-9c5c9712527c // indirect
+	github.com/juju/cmd v0.0.0-20200108104440-8e43f3faa5c9
+	github.com/juju/errors v0.0.0-20190930114154-d42613fe1ab9
+	github.com/juju/gnuflag v0.0.0-20171113085948-2ce1bb71843d
+	github.com/juju/loggo v0.0.0-20190526231331-6e530bcce5d8
+	github.com/juju/retry v0.0.0-20180821225755-9058e192b216 // indirect
+	github.com/juju/testing v0.0.0-20191001232224-ce9dec17d28b // indirect
+	github.com/juju/utils v0.0.0-20200116185830-d40c2fe10647 // indirect
+	github.com/juju/version v0.0.0-20191219164919-81c1be00b9a6 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/lunixbochs/vtclean v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/crypto v0.0.0-20200128174031-69ecbb4d6d5d // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package main
+
+import (
+	"os"
+
+	"github.com/juju/cmd"
+	"github.com/juju/errors"
+	"github.com/juju/gnuflag"
+	"github.com/juju/loggo"
+)
+
+var logger = loggo.GetLogger("juju-restore")
+
+const defaultLogConfig = "<root>=DEBUG"
+
+var loggingConfig = defaultLogConfig
+
+func main() {
+	_, err := loggo.ReplaceDefaultWriter(cmd.NewWarningWriter(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(Run(os.Args))
+}
+
+// Run creates and runs the restore command.
+func Run(args []string) int {
+	ctx, err := cmd.DefaultContext()
+	if err != nil {
+		logger.Errorf("%v", err)
+		return 2
+	}
+
+	restorer := NewRestoreCommand(ctx)
+	return cmd.Main(restorer, ctx, args[1:])
+}
+
+// NewRestoreCommand creates a cmd.Command to check the database and
+// restore the Juju backup.
+func NewRestoreCommand(ctx *cmd.Context) cmd.Command {
+	return &restoreCommand{}
+}
+
+type restoreCommand struct {
+	cmd.CommandBase
+
+	hostname string
+	port     string
+	ssl      bool
+	username string
+	password string
+
+	backupFile string
+}
+
+// Info is part of cmd.Command.
+func (c *restoreCommand) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "juju-restore",
+		Args:    "<backup file>",
+		Purpose: "Restore a Juju backup file into a specified controller",
+		Doc:     restoreDoc,
+	}
+}
+
+// SetFlags is part of cmd.Command.
+func (c *restoreCommand) SetFlags(f *gnuflag.FlagSet) {
+	c.CommandBase.SetFlags(f)
+	f.StringVar(&c.hostname, "hostname", "localhost", "hostname of the Juju MongoDB server")
+	f.StringVar(&c.port, "port", "37017", "port of the Juju MongoDB server")
+	f.BoolVar(&c.ssl, "ssl", true, "use SSL to connect to MongoDB")
+	f.StringVar(&c.username, "username", "admin", "user for connecting to MongoDB (\"\" for no authentication)")
+	f.StringVar(&c.password, "password", "", "password for connecting to MongoDB")
+	f.StringVar(&loggingConfig, "logging-config", defaultLogConfig, "set logging levels")
+}
+
+// Init is part of cmd.Command.
+func (c *restoreCommand) Init(args []string) error {
+	if len(args) == 0 {
+		return errors.New("missing backup file")
+	}
+	c.backupFile, args = args[0], args[1:]
+	return c.CommandBase.Init(args)
+}
+
+// Run is part of cmd.Command.
+func (c *restoreCommand) Run(ctx *cmd.Context) error {
+	return nil
+}
+
+const restoreDoc = `
+
+juju-restore must be executed on the MongoDB primary host of a Juju
+controller.
+
+The command will check the state of the target database and the
+details of the backup file provided, and restore the contents of the
+backup into the controller database.
+
+`
